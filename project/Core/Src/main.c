7c2298f5d2b1d5c5c9b40ca66672a846ead952f4
saacifuentesmu <saacifuentesmu@unal.edu.co>
2023-11-09 18:53:01 -0500

    feat(keypad): library implemented and tested.

--- project/Core/Src/main.c
+++ project/Core/Src/main.c
@@ -24,6 +24,7 @@
 #include <stdio.h>
 
 #include "ring_buffer.h"
+#include "keypad.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -92,90 +93,6 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	key_event = GPIO_Pin;
 }
-
-/**
- * @brief This functions initialize the functionality of the keypad
- */
-void keypad_init(void)
-{
-	/* Set the rows high to be detected in the columns by rising interrupt */
-	ROW_1_GPIO_Port->BSRR = ROW_1_Pin;
-	ROW_2_GPIO_Port->BSRR = ROW_2_Pin;
-	ROW_3_GPIO_Port->BSRR = ROW_3_Pin;
-	ROW_4_GPIO_Port->BSRR = ROW_4_Pin;
-}
-
-/**
- * @brief  This function debounces and identify keypad events.
- * @param  column_to_evaluate: the column where the event happened.
- * @retval 0xFF -> invalid key. [0x00 - 0x0F] -> valid key.
- */
-uint8_t keypad_handler(uint16_t column_to_evaluate)
-{
-	uint8_t key_pressed = 0xFF; // Value to return
-
-	/*** Debounce the key press (remove noise in the key) ***/
-#define KEY_DEBOUNCE_MS 300 /*!> Minimum time required for since last press */
-	static uint32_t last_pressed_tick = 0;
-	if (HAL_GetTick() <= (last_pressed_tick + KEY_DEBOUNCE_MS)) {
-		// less than KEY_DEBOUNCE_MS since last press. Probably noise
-		return key_pressed; // return 0xFF
-	}
-	last_pressed_tick = HAL_GetTick();
-
-	/*** Check in which column the event happened ***/
-	switch (column_to_evaluate) {
-	case COLUMN_1_Pin:
-		ROW_1_GPIO_Port->BSRR = ROW_1_Pin; // turn on row 1
-		ROW_2_GPIO_Port->BRR = ROW_2_Pin;  // turn off row 2
-		ROW_3_GPIO_Port->BRR = ROW_3_Pin;  // turn off row 3
-		ROW_4_GPIO_Port->BRR = ROW_4_Pin;  // turn off row 4
-		HAL_Delay(2); // wait for voltage to establish
-		if (COLUMN_1_GPIO_Port->IDR & COLUMN_1_Pin) {
-			key_pressed = 0x01; // if column 1 is still high -> column 1 + row 1 = key 1
-			break;
-		}
-
-		ROW_1_GPIO_Port->BRR = ROW_1_Pin; 	// turn off row 1
-		ROW_2_GPIO_Port->BSRR = ROW_2_Pin; 	// turn on row 2
-		HAL_Delay(2); // wait for voltage to establish
-		if (COLUMN_1_GPIO_Port->IDR & COLUMN_1_Pin) {
-			key_pressed = 0x04; // if column 1 is still high -> column 1 + row 2 = key 4
-			break;
-		}
-
-		ROW_2_GPIO_Port->BRR = ROW_2_Pin; 	// turn off row 2
-		ROW_3_GPIO_Port->BSRR = ROW_3_Pin; 	// turn on row 3
-		HAL_Delay(2); // wait for voltage to establish
-		if (COLUMN_1_GPIO_Port->IDR & COLUMN_1_Pin) {
-			key_pressed = 0x07; // if column 1 is still high -> column 1 + row 3 = key 7
-			break;
-		}
-
-		ROW_3_GPIO_Port->BRR = ROW_3_Pin;	// turn off row 3
-		ROW_4_GPIO_Port->BSRR = ROW_4_Pin; 	// turn on row 4
-		HAL_Delay(2); // wait for voltage to establish
-		if (COLUMN_1_GPIO_Port->IDR & COLUMN_1_Pin) {
-			key_pressed = 0x0E; // if column 1 is still high -> column 1 + row 4 = key *
-			break;
-		}
-	  break;
-
-	case COLUMN_2_Pin:
-		/*!\ TODO: Implement validation for column 2 here */
-		break;
-
-	/*!\ TODO: Implement other column cases here */
-
-	default:
-		/* This should not be reached */
-		printf("Unknown column: %x\r\n", column_to_evaluate);
-	  break;
-	}
-
-	keypad_init(); // set the columns high again
-	return key_pressed; // invalid: 0xFF, valid:[0x00-0x0F]
-}
 /* USER CODE END 0 */
 
 /**
@@ -220,9 +137,9 @@ int main(void)
   while (1)
   {
 	  if (key_event != 0xFF) { // check if there is a event from the EXTi callback
-		  uint16_t key_pressed = keypad_handler(key_event); // call the keypad handler
+		  uint8_t key_pressed = keypad_handler(key_event); // call the keypad handler
 		  if (key_pressed != 0xFF) {
-			  printf("Key pressed: %x\r\n", key_pressed); // print the key pressed
+			  printf("Key pressed: %c\r\n", key_pressed); // print the key pressed
 		  }
 		  key_event = 0xFF; // clean the event
 	  }
